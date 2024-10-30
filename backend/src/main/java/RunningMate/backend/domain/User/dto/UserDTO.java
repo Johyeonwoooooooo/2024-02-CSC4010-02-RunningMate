af804c4fc9d497e953673291d6eaf91a86554da1
changyunyeong <sally0109277@naver.com>
2024-10-30 18:54:59 +0900

    fix: annotation 추가

--- backend/src/main/java/RunningMate/backend/domain/User/dto/UserDTO.java
+++ backend/src/main/java/RunningMate/backend/domain/User/dto/UserDTO.java
@@ -1,13 +1,18 @@
 package RunningMate.backend.domain.User.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 //https://github.com/Like-House/BE/blob/develop/src/main/java/backend/like_house/domain/user/dto/UserDTO.java 참고해서 만듦
+@AllArgsConstructor
 public class UserDTO {
 
     @Builder
     @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class LoginRequest{
         private String userEmail;
         private String userPassword;
@@ -15,6 +20,8 @@ public class UserDTO {
 
     @Builder
     @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class SignUpRequest {
         private String userNickname;
         private String userEmail;
@@ -25,6 +32,8 @@ public class UserDTO {
 
     @Builder
     @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class GetProfileResponse {
         private String userNickname;
         private Long userWeight;
@@ -33,6 +42,8 @@ public class UserDTO {
 
     @Getter
     @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class UpdateProfileRequest {
         private Long userWeight;
         private Long userHeight;
