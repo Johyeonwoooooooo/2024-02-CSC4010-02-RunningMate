6ff8d2214eec414e0e35a3a00c35a8301aa62756
kohs2k21 <gw010101@naver.com>
2024-11-01 00:24:05 +0900

    feat: 로그인 페이지 레이아웃 구현

--- frontend/runningmate/app/LoginScreen.js
+++ frontend/runningmate/app/LoginScreen.js
@@ -2,51 +2,110 @@ import React from "react";
 import {
   View,
   Text,
-  Button,
-  ScrollView,
+  TextInput,
   SafeAreaView,
   StyleSheet,
+  Image,
 } from "react-native";
 import { useRouter } from "expo-router";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
 import CustomButton from "../components/CustomButton";
+import PasswordInput from "../components/PasswordInput";
 
 const LoginScreen = () => {
   const router = useRouter();
 
   const handleLogin = () => {
-    // 로그인 로직을 여기에 추가합니다.
-    // 로그인 성공 시 (tabs)로 전환합니다.
-    // router.replace("(tabs)");
-    router.navigate("(tabs)");
+    /* TODO: 로그인 처리 */
+
+    // router.replace("(tabs)"); // 이전 페이지로 이동 불가능
+    router.navigate("(tabs)"); // 이전 페이지로 스와이프하여 이동 가능 (테스트 용이)
   };
 
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <ScrollView contentContainerStyle={styles.container}>
-        <CustomButton title="로그인" onPress={handleLogin} />
+    <View style={styles.container}>
+      <SafeAreaView style={styles.safeArea}>
+        {/* 로고 */}
+        <Image
+          source={require("../assets/icons/loginLogo.png")}
+          style={styles.logo}
+        />
+
+        {/* Welcome 메시지 */}
+        <Text style={styles.title}>
+          Running Mate에 오신 것을 {"\n"} 환영합니다!
+        </Text>
+
+        {/* 이메일 입력 필드 */}
+        <TextInput
+          style={styles.input}
+          placeholder="이메일"
+          placeholderTextColor="#6e6e6e"
+        />
+
+        {/* 비밀번호 입력 필드 */}
+        <PasswordInput style={styles.passwordInput} />
+
+        {/* 버튼 */}
+        <CustomButton
+          title="로그인"
+          onPress={handleLogin}
+          buttonStyle="skyblue"
+        />
         <CustomButton
           title="회원가입"
-          onPress={() => router.navigate("Register")}
-          buttonColor="#fff"
-          borderColor="#8dccff"
-          textColor="#8dccff"
+          onPress={() => router.navigate("RegisterScreen")}
+          buttonStyle="white"
+          style={{ marginTop: 10 }}
         />
-      </ScrollView>
-    </SafeAreaView>
+      </SafeAreaView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  safeArea: {
+  container: {
     flex: 1,
     backgroundColor: "#fff",
   },
-  container: {
-    flexGrow: 1,
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#fff",
     justifyContent: "center",
+    alignItems: "center",
     padding: 16,
+    width: "80%",
+    height: "100%",
+    alignSelf: "center",
+  },
+  logo: {
+    width: "100%",
+    height: "15%",
+    aspectRatio: 552 / 375,
+    marginBottom: 20,
+  },
+  title: {
+    color: "#000",
+    textAlign: "center",
+    marginBottom: 20,
+    paddingVertical: 10,
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  input: {
+    width: 300,
+    height: 45,
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    alignSelf: "center",
+    marginTop: 10,
+    paddingLeft: 20,
+    borderColor: "#F2F4F7",
+    backgroundColor: "#F2F4F7",
+  },
+  toggleButton: {
+    marginLeft: 10,
   },
 });
 
--- frontend/runningmate/app/_layout.tsx
+++ frontend/runningmate/app/_layout.tsx
@@ -4,27 +4,40 @@ import {
   ThemeProvider,
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
+import { AuthProvider, useAuth } from "../context/AuthContext";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
+  const router = useRouter();
+  const { isLoggedIn } = useAuth();
 
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
     }
   }, [loaded]);
+
+  useEffect(() => {
+    if (loaded) {
+      if (isLoggedIn) {
+        router.replace("/(tabs)");
+      } else {
+        router.replace("/LoginScreen");
+      }
+    }
+  }, [isLoggedIn, loaded]);
 
   if (!loaded) {
     return null;
@@ -35,8 +48,16 @@ export default function RootLayout() {
       <Stack>
         <Stack.Screen name="LoginScreen" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        {/* <Stack.Screen name="+not-found" /> */}
       </Stack>
     </ThemeProvider>
+  );
+}
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <RootLayout />
+    </AuthProvider>
   );
 }
