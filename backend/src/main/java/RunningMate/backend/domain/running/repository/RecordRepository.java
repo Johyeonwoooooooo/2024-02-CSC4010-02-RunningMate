74366ef3dadad479244da5d18f3d198ecc303f3c
Johyeonwoooooooo <hyunwoo02031@naver.com>
2024-11-05 17:30:55 +0900

    fix: Repository 에러 수정

--- backend/src/main/java/RunningMate/backend/domain/running/repository/RecordRepository.java
+++ backend/src/main/java/RunningMate/backend/domain/running/repository/RecordRepository.java
@@ -1,5 +1,6 @@
 package RunningMate.backend.domain.running.repository;
 
+import RunningMate.backend.domain.running.entity.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecordRepository extends JpaRepository<Record, Long> {
