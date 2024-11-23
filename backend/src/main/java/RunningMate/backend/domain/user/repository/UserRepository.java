cecaca4b7e18130a0a4c9157effc9d70a48abaa4
changyunyeong <sally0109277@naver.com>
2024-11-23 21:03:01 +0900

    feat: 주간 기록 확인

--- backend/src/main/java/RunningMate/backend/domain/user/repository/UserRepository.java
+++ backend/src/main/java/RunningMate/backend/domain/user/repository/UserRepository.java
@@ -3,6 +3,7 @@ package RunningMate.backend.domain.user.repository;
 import RunningMate.backend.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
