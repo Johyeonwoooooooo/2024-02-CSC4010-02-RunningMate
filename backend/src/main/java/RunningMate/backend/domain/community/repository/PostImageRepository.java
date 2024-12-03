3ff16474741e3a272bdf33094cb8b9b497e43547
SeongJun <67622215+kohs2k21@users.noreply.github.com>
2024-12-03 15:52:50 +0900

    refactor: Delete backend directory

--- backend/src/main/java/RunningMate/backend/domain/community/repository/PostImageRepository.java
+++
@@ -1,7 +0,0 @@
-package RunningMate.backend.domain.community.repository;
-
-import RunningMate.backend.domain.community.entity.PostImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostImageRepository extends JpaRepository<PostImage, Long> {
-}
